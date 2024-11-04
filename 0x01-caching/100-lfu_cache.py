6a3e9e75db63569f41a8c3c79b2729d1c5c320ca
bashar-karoury <basharalkaroury@hotmail.com>
2024-11-04 20:58:29 +0300

    Update: 100-lfu_cache.py

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -10,6 +10,7 @@ class LFUCache(BaseCaching):
     """ LFUCache class:
         caching system following LFU caching policy
     """
+    recent_counter = 0
 
     def __init__(self):
         """ Initiliaze
@@ -31,7 +32,7 @@ class LFUCache(BaseCaching):
             if length >= self.MAX_ITEMS:
                 # get LRU element
                 sorted_freq = dict(
-                    sorted(self.__usage_freq.items(), key=lambda it: it[1]))
+                    sorted(self.__usage_freq.items(), key=lambda it: (it[1]['frequency'], it[1]['recent'])))
                 discarded_key = list(sorted_freq)[0]
                 # delete from cache
                 del self.cache_data[discarded_key]
@@ -51,7 +52,16 @@ class LFUCache(BaseCaching):
         return result
 
     def update(self, key):
-        """ update usage freq for key"""
-        freq = self.__usage_freq.get(key)
+        """ update usage freq and recent for key"""
+        exist_key = self.__usage_freq.get(key)
+        # print('freq---------', exist_key)
 
-        self.__usage_freq[key] = 1 if freq is None else freq + 1
+        # self.__usage_freq[key].frequency = 1 if freq is None else freq + 1
+        if exist_key:
+            self.__usage_freq[key]['frequency'] = self.__usage_freq[key]['frequency'] + 1
+        else:
+            self.__usage_freq[key] = {}
+            self.__usage_freq[key]['frequency'] = 1
+        self.__usage_freq[key]['recent'] = 1
+        LFUCache.recent_counter += 1
+        # print(self.__usage_freq[key])
