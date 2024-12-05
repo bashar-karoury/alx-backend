9a0a681360c94bea1fed7d3755fc34f1f14d5d18
bashar_karoury <basharalkaroury@hotmail.com>
2024-12-05 09:51:43 +0300

    Updated files: 6-job_creator.js

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,24 @@
+import { createClient } from 'redis';
+import kue from 'kue';
+
+const client = createClient();
+const push_notification_code =  kue.createQueue();
+const obj = {
+    phoneNumber: '0907164806',
+    message: 'phone number string',
+  };
+const job = push_notification_code.create(obj).save((err) => {
+    if (err) {
+      console.log('Error saving job:', err);
+    } else {
+      console.log(`Notification job created: ${job.id}`);
+    }
+});
+
+// Set up event listeners for the job
+job.on('complete', () => {
+  console.log('Notification job completed');
+});
+job.on('failed', (err) => {
+  console.log(`Notification job failed: ${err}`);
+});
